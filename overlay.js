73ee73f2c673725febb52b3d0f66d91c9a219a97
Josh Emerson <josh.j.emerson@gmail.com>
2011-07-15 17:18:43 +0100

    HTML, CSS and JS separated.

--- overlay.js
+++ overlay.js
@@ -3,12 +3,23 @@ function overlayText(selector) {
     var t = document.createElement('div');
     // If the browser supports the 'content' CSS property
     if ('content' in t.style) {
+        // Create general style for laying out the overlay elements
+        var s = document.createElement('style');
+        var c = ".overlay { position: relative; } .overlay:after { position: absolute; top: 0; left: 0; z-index: 10; content: attr(data-content); }";
+        if (s.styleSheet) {
+            s.styleSheet.cssText = c;
+        } else {
+            s.appendChild(document.createTextNode(c));
+        }
+        // Add the style to the head
+        document.getElementsByTagName("head")[0].appendChild(s);
         // Add the class has-overlay to the body
         document.body.className += ' has-overlay';
         // Get a list of elements matching the selector
         var e = document.querySelectorAll(selector);
         // For each element
         for (var i = 0; i < e.length; i++) {
+            e[i].className += ' overlay';
             // Put the element's contents in our testing div
             t.innerHTML = e[i].innerHTML;
             // so that we can extract the plain text version from the div
